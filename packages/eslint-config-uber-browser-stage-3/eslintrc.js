d794f510ef2c4d5808e604f539db770dafe9a12b
David Kearns <dave.p.kearns@gmail.com>
2017-07-14 11:29:17 -0700

    Add new eslint-configs

---
+++ packages/eslint-config-uber-browser-stage-3/eslintrc.js
@@ -0,0 +1,11 @@
+module.exports = {
+
+  extends: [
+    require.resolve('eslint-config-uber-base-stage-3')
+  ],
+
+  env: {
+    browser: true
+  }
+
+};
---
+++ packages/eslint-config-uber-universal-stage-3/eslintrc.js
@@ -0,0 +1,20 @@
+module.exports = {
+
+  extends: [
+    require.resolve('eslint-config-uber-base-stage-3'),
+    require.resolve('eslint-config-cup'),
+  ],
+
+  plugins: [
+    'eslint-plugin-cup'
+  ],
+
+  rules: {
+    /**
+     * Custom `no-undef` rule to allow appropriate globals within statically analyzable
+     * `if (__NODE__)` or `if (__BROWSER__)` or `if (__DEV__)` checks
+     */
+    'no-undef': 'off',
+    'cup/no-undef': ['error'],
+  }
+};
